554f0ee1f6351c8810f66a36fbbd849a21fd1ed1
Jose Lozano <joselozano2003@Joses-MacBook-Pro.local>
2023-07-04 19:57:29 -0600

    2/3 steps in log in

---
+++ src/lib/firebase.ts
@@ -0,0 +1,96 @@
+import { initializeApp } from "firebase/app";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { writable, type Readable, derived } from "svelte/store";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBQidCBbggy_5jMdjhOcFBteB5rVB-6IiE",
+    authDomain: "linktree-clone-92fcf.firebaseapp.com",
+    projectId: "linktree-clone-92fcf",
+    storageBucket: "linktree-clone-92fcf.appspot.com",
+    messagingSenderId: "313146090103",
+    appId: "1:313146090103:web:78cfd9b7b5017620589c72",
+    measurementId: "G-PRNK8KQXN4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
+export const storage = getStorage();
+
+
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+    let unsubscribe: () => void;
+
+    if (!auth || !globalThis.window) {
+        console.warn('Auth is not initialized or not in browser');
+        const { subscribe } = writable<User | null>(null);
+        return {
+        subscribe,
+        }
+    }
+
+    const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+        unsubscribe = onAuthStateChanged(auth, (user) => {
+        set(user);
+        });
+
+    return () => unsubscribe();
+    });
+
+    return {
+        subscribe,
+    };
+}
+
+export const user = userStore();
+
+
+
+/**
+ * @param  {string} path document path or reference
+ * @param  {any} startWith optional default data
+ * @returns a store with realtime updates on document data
+ */
+export function docStore<T>(
+    path: string,
+) {
+    let unsubscribe: () => void;
+
+    const docRef = doc(db, path);
+
+    const { subscribe } = writable<T | null>(null, (set) => {
+        unsubscribe = onSnapshot(docRef, (snapshot) => {
+        set((snapshot.data() as T) ?? null);
+        });
+
+        return () => unsubscribe();
+    });
+
+    return {
+        subscribe,
+        ref: docRef,
+        id: docRef.id,
+    };
+}
+
+interface UserData {
+    username: string;
+    bio: string;
+    photoURL: string;
+    published: boolean;
+    links: any[];
+}
+
+export const userData: Readable<UserData | null> = derived(user, ($user, set) => { 
+    if ($user) {
+        return docStore<UserData>(`users/${$user.uid}`).subscribe(set);
+    } else {
+        set(null); 
+    }
+});  
---
+++ src/routes/login/username/+page.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+    import AuthCheck from "$lib/components/AuthCheck.svelte";
+    import { db, user, userData } from "$lib/firebase";
+    import { doc, getDoc, writeBatch } from "firebase/firestore";
+
+    let username = "";
+    let loading: boolean = false;
+    let isAvailable: boolean = false;
+
+    let debounceTimer: NodeJS.Timeout;
+
+    async function checkAvailability(){
+        isAvailable = false;
+        clearTimeout(debounceTimer);
+        loading = true;
+
+        debounceTimer = setTimeout(async() => {
+            console.log("Checking availability for", username);
+
+            const ref = doc(db, "usernames", username);
+            const exists = await getDoc(ref).then((doc) => doc.exists());
+
+            isAvailable = !exists;
+            loading = false;
+        }, 500);
+    }
+
+    async function confirmUsername(){
+        console.log("Confirming username", username);
+        const batch = writeBatch(db);
+        batch.set(doc(db, "usernames", username), { uid: $user?.uid });
+        batch.set(doc(db, "users", $user!.uid), { 
+            username, 
+            photoURL: $user?.photoURL ?? null,
+            published: true,
+            bio: 'I am the Walrus',
+            links: [
+                {
+                title: 'Test Link',
+                url: 'https://kung.foo',
+                icon: 'custom'
+                }
+            ]
+        });
+
+        await batch.commit();
+
+        username = '';
+        isAvailable = false;
+    }
+    const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+  
+    $: isValid = username?.length > 2 && username.length < 16 && re.test(username);
+    $: isTouched = username.length > 0;
+    $: isTaken = isValid && !isAvailable && !loading
+  
+</script>
+
+<AuthCheck>
+    {#if $userData?.username}
+        <p class="text-lg">
+        Your username is <span class="text-success font-bold"
+            >@{$userData.username}</span
+        >
+        </p>
+        <p class="text-sm">(Usernames cannot be changed)</p>
+        <a class="btn btn-primary" href="/login/photo">Upload Profile Image</a>
+    {:else}
+    <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+        <input
+            type="text"
+            placeholder="Username"
+            class="input w-full"
+            bind:value={username}
+            on:input={checkAvailability}
+            class:input-error={(!isValid && isTouched)}
+            class:input-warning={isTaken}
+            class:input-success={isAvailable && isValid && !loading}
+        />
+
+        <div class="my-4 min-h-16 px-8 w-full">
+            {#if loading}
+              <p class="text-secondary">Checking availability of @{username}...</p>
+            {/if}
+        
+            {#if !isValid && isTouched}
+              <p class="text-error text-sm">
+                must be 3-16 characters long, alphanumeric only
+              </p>
+            {/if}
+        
+            {#if isValid && !isAvailable && !loading}
+              <p class="text-warning text-sm">
+                @{username} is not available
+              </p>
+            {/if}
+        
+            {#if isAvailable}
+              <button class="btn btn-success">Confirm username @{username} </button>
+            {/if}
+          </div>
+    </form>
+    {/if}
+</AuthCheck>
