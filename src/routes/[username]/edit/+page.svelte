afe52fe22ad68a8d0291947779bb8d8828b2e227
Jose Lozano <joselozano2003@Joses-MacBook-Pro.local>
2023-07-05 21:00:10 -0600

    fixed label

--- src/routes/[username]/edit/+page.svelte
+++ src/routes/[username]/edit/+page.svelte
@@ -94,7 +94,7 @@
 				href={`/${$userData?.username}`}
 				class="link link-accent"
 			>
-			https://linktree-clone-plum.vercel.app/{$userData?.username}
+			https://link-shelf.vercel.app/{$userData?.username}
 			</a>
 			</p>
 		</div>
